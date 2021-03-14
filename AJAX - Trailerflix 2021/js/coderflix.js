1c79683dc4f099b3873e80f891800f1a251e4d3d
Fernando O <fernando@vidamobile.com.ar>
2021-03-14 19:14:16 -0300

    Optimizing loader

--- AJAX - Trailerflix 2021/js/coderflix.js
+++ AJAX - Trailerflix 2021/js/coderflix.js
@@ -19,37 +19,35 @@ function cargoContenidoStreaming() {
 function buildCard(ps) {
    let HTMLCard = ""
    let SerieOpelicula = ""
-   if(ps.categoria == "Serie") {
-      SerieOpelicula = `<p class="yellow-text">TEMPORADAS: <span class="white-text">${ps.temporadas}</span></p>`
-   } else {
-      SerieOpelicula = `<p class="yellow-text">DURACIÓN: <span class="white-text">${ps.duracion}</span></p>`
-   }
-   HTMLCard += `<div class="col s12 m6 l3">
-                   <div class="card z-depth-2">
-                     <div class="card-image">
-                        <img src="${ps.poster}" alt="${ps.titulo}" id="trailerflix-"${ps.id} max-width="100px">
-                        <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons" onclick="verDetalle(${ps.id})">search</i></a>
-                     </div>
-                     <div class="card-content black">
-                        <p class="yellow-text">GÉNERO: <span class="white-text">${ps.genero}</span></p>
-                        ${SerieOpelicula}
-                     </div>
-                   </div>
-                </div>`
-   return HTMLCard
+       SerieOpelicula = `<p class="yellow-text">DURACIÓN: <span class="white-text">${ps.duracion}</span></p>`
+       if (ps.categoria == "Serie")
+           SerieOpelicula = `<p class="yellow-text">TEMPORADAS: <span class="white-text">${ps.temporadas}</span></p>`
+       HTMLCard += `<div class="col s12 m6 l3">
+                      <div class="card z-depth-2">
+                         <div class="card-image">
+                            <img src="${ps.poster}" alt="${ps.titulo}" id="trailerflix-"${ps.id} max-width="100px">
+                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons"  onclick="verDetalle(${ps.id})">search</i></a>
+                         </div>
+                         <div class="card-content black">
+                            <p class="yellow-text">GÉNERO: <span class="white-text">${ps.genero}</span></p>
+                            ${SerieOpelicula}
+                         </div>
+                      </div>
+                   </div>`
+       return HTMLCard
 }
 
 function errorJSON() {
    let HTMLCard = ""
-   console.error("Ocurrió un error... :(")
-   HTMLCard = `<div class="center white-text"> 
-                  <br><br> 
-                  <h4>El contenido parece no estar disponible. Intente nuevamente en unos minutos.</h4> 
-                  <br><br> 
-                  <i class="large material-icons">sentiment_very_dissatisfied</i> 
-                  <br><br> 
-               </div>`
-   return HTMLCard
+       console.error("Ocurrió un error... :(")
+       HTMLCard = `<div class="center white-text"> 
+                      <br><br> 
+                      <h4>El contenido parece no estar disponible. Intente nuevamente en unos minutos.</h4> 
+                      <br><br> 
+                      <i class="large material-icons">sentiment_very_dissatisfied</i> 
+                      <br><br> 
+                   </div>`
+       return HTMLCard
 }
 
 setTimeout(() => {
