ffdf1f23a70c3878e8927d38265471c988407325
Fernando O <fernando@vidamobile.com.ar>
2021-03-14 19:07:19 -0300

    New version of TrailerFlix

---
+++ AJAX - Trailerflix 2021/js/coderflix.js
@@ -0,0 +1,74 @@
+let contenidoJSON = []
+
+function cargoContenidoStreaming() {
+   $.ajax({
+      url: "js/api.json",
+      dataType: "json",
+      success: function(contenidoJSON) {
+         localStorage.contenidoJSON = JSON.stringify(contenidoJSON)
+         $.each(contenidoJSON, function(i) {
+            $("#contenido").append(buildCard(contenidoJSON[i]))
+         })
+      },
+      error: function() {
+         $("#contenido").html(errorJSON())
+      } 
+   })
+}
+
+function buildCard(ps) {
+   let HTMLCard = ""
+   let SerieOpelicula = ""
+   if(ps.categoria == "Serie") {
+      SerieOpelicula = `<p class="yellow-text">TEMPORADAS: <span class="white-text">${ps.temporadas}</span></p>`
+   } else {
+      SerieOpelicula = `<p class="yellow-text">DURACIÓN: <span class="white-text">${ps.duracion}</span></p>`
+   }
+   HTMLCard += `<div class="col s12 m6 l3">
+                   <div class="card z-depth-2">
+                     <div class="card-image">
+                        <img src="${ps.poster}" alt="${ps.titulo}" id="trailerflix-"${ps.id} max-width="100px">
+                        <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons" onclick="verDetalle(${ps.id})">search</i></a>
+                     </div>
+                     <div class="card-content black">
+                        <p class="yellow-text">GÉNERO: <span class="white-text">${ps.genero}</span></p>
+                        ${SerieOpelicula}
+                     </div>
+                   </div>
+                </div>`
+   return HTMLCard
+}
+
+function errorJSON() {
+   let HTMLCard = ""
+   console.error("Ocurrió un error... :(")
+   HTMLCard = `<div class="center white-text"> 
+                  <br><br> 
+                  <h4>El contenido parece no estar disponible. Intente nuevamente en unos minutos.</h4> 
+                  <br><br> 
+                  <i class="large material-icons">sentiment_very_dissatisfied</i> 
+                  <br><br> 
+               </div>`
+   return HTMLCard
+}
+
+setTimeout(() => {
+   cargoContenidoStreaming()
+   $('#contenido').fadeIn("fast", function() {
+      $('#cargando').fadeOut(200)
+   })
+}, 2000)
+
+function verDetalle(i) {
+   if (contenidoJSON.length == 0)
+      contenidoJSON = JSON.parse(localStorage.contenidoJSON)
+   let detalleJSON = contenidoJSON.find(item => item.id == i)
+      if (detalleJSON != undefined) {
+         localStorage.setItem("contenido", JSON.stringify(detalleJSON))
+         location.href = "detail.html"
+         console.log(detalleJSON)
+      } else {
+         console.error("No se encontró el elemento: " + i)
+         M.toast({html: "Contenido temporalmente no disponible.", classes: "red darken-3 white-text"})
+      }
+}
---
+++ AJAX - Trailerflix 2021/js/contentdetail.js
@@ -0,0 +1,64 @@
+let HTMLTable = ""
+let HTMLtrailer = ""
+let informacionDeContenido = ""
+
+$(document).ready(function() {
+   if (localStorage.contenido != undefined) {
+      informacionDeContenido = JSON.parse(localStorage.contenido)
+      HTMLTable = `<div class="row center">
+                     <div class="col s12 m5 l4 center-align">
+                        <img src="${informacionDeContenido.poster}" width="200px">
+                     </div>
+                     <div class="col s12 m7 l8">
+                     <table>
+                        <tbody class="white-text">
+                           <tr>
+                           <td class="yellow-text">TÍTULO</td>
+                           <td><h5>${informacionDeContenido.titulo}</h5></td>
+                           </tr>
+                           <tr>
+                           <td class="yellow-text">CATEGORÍA</td>
+                           <td>${informacionDeContenido.categoria}</td>
+                           </tr>
+                           <tr>
+                           <td class="yellow-text">GÉNERO</td>
+                           <td>${informacionDeContenido.genero}</td>
+                           </tr>
+                           <tr>
+                           <td class="yellow-text">RESUMEN</td>
+                           <td>${informacionDeContenido.resumen}</td>
+                           </tr>
+                           <tr>
+                           <td class="yellow-text">TEMPORADAS</td>
+                           <td>${informacionDeContenido.temporadas}</td>
+                           </tr>
+                           <tr>
+                           <td class="yellow-text">REPARTO</td>
+                           <td><h6>${informacionDeContenido.reparto}</h6></td>
+                           </tr>
+                        </tbody>
+                     </table>
+                     </div>
+                     </div>`
+      if (informacionDeContenido.trailer > "")
+         HTMLtrailer = `<div class="video-container">
+                           <iframe width="560" height="315" src="${informacionDeContenido.trailer}" frameborder="0" encrypted-media; allowfullscreen></iframe>
+                        </div>`
+         $("#trailer").html(HTMLtrailer)
+   }
+   $('#contenido').html(HTMLTable)
+})
+
+$(window).ready(function() {
+   $('h1').fadeIn(800, function() {
+      $('h4').fadeIn(500)
+      $('#contenido').slideDown("slow", function() {
+         $('#trailer').fadeIn()
+      })
+   })
+
+   $("#retornar").click(function() {
+      location.target = "_self"
+      location.href = "index.html"
+   })
+})
