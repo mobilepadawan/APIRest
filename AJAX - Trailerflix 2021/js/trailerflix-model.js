01feefbbae6f04a9ee689e88fd7265c2f94a6e7f
Fernando O <fernando@vidamobile.com.ar>
2021-05-30 21:12:16 -0300

    Update trailerflix-model.js

--- AJAX - Trailerflix 2021/js/trailerflix-model.js
+++ AJAX - Trailerflix 2021/js/trailerflix-model.js
@@ -1,3 +1,5 @@
 let contenidoJSON = []
 const GEN = ["Acción", "Aventura", "Ciencia Ficción", "Comedia", "Drama", "Familia", "Fantasía", "Suceso Real", "Suspenso", "Terror"]
 const URL_CONTENIDO = "js/trailerflix.json"
+
+const APPLELOGO = ""
